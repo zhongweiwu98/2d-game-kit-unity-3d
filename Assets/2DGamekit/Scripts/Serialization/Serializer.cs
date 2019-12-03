79222cbe421cfb4caacba4c87ee8148ad8de568c
awuwu <wzw_98@msn.com>
2019-12-03 15:28:08 +0800

    add serializer.cs

---
+++ Assets/2DGamekit/Scripts/Serialization/Serializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets._2DGamekit.Scripts.Serialization
+{
+    class Serializer
+    {
+    }
+}
